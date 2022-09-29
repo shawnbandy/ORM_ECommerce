cc50c1f448c5640e6b2e0ff6550424c6640746c1
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-09-29 16:33:27 -0400

    did some routing working, trying to bugfix

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,32 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const router = require('../routes');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+
 
 module.exports = {
   Product,
